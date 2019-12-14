1412e1021c6928ac064b6f8283eacee05607d873
Vic Shóstak <vikkyshostak@gmail.com>
2019-12-14 23:12:54 +0300

    Fight with certbot script

--- configs/init-letsencrypt.sh
+++ configs/init-letsencrypt.sh
@@ -63,6 +63,9 @@ echo "### Starting nginx ..."
 # Restarting for case if nginx container is already started
 docker-compose up -d nginx && docker-compose restart nginx
 
+echo "### Test nginx ..."
+curl http://$domain_name
+
 # Select appropriate email arg
 case "$email" in
   "") email_arg="--register-unsafely-without-email" ;;
